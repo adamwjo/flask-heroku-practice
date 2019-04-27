517ff3a99e5191e3a6a15891ebda7ab1b9b03a0d
adamwjo <adamwjo@gmail.com>
2019-04-27 12:21:12 -0400

    set up for heroku

---
+++ src/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import os
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    return render_template('show.html')
+
+
+
+
+
